1fd1469a272153c66f219b5fb38cfd1530d8d620
RakAMih <2304alex2304@gmail.com>
2020-06-25 19:33:04 +0500

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f https://github.com/alex-rak/alex-rak.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f https://alex-rah.github.io/search-youtube git master:gh-pages
+git push -f https://alex-rah.github.io/search-youtube.git master:gh-pages
 
 cd -
